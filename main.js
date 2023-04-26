f5dbe6a99c90a51cb6ac8e8b7c1cb04499f043fb
david-senra <72723579+david-senra@users.noreply.github.com>
2023-04-25 21:54:40 -0300

    Edit main.js
    Modificação no código visando uma forma mais inteligente de vincular a tarefa clicada à função de rabiscar.

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
     
-    let contador = 0;
-    
     $('form').on('submit', function (e) {
         e.preventDefault();
         $('ul h2').css("display", "block");
         $('#limpar-lista').css("display", "inline");
-        contador += 1;
         const atividadeEscrita = $('#tarefa-definida').val();
         const novoitem = $(`<li></li>`);
-        console.log(`${atividadeEscrita}`);
-        $(`<p id="${contador}">${atividadeEscrita}</p>`).appendTo(novoitem);
+        $(`<p>${atividadeEscrita}</p>`).appendTo(novoitem);
         novoitem.appendTo($('.listagem'));
         $(novoitem).fadeIn(1500);
-        $(novoitem).attr('onClick', `rabiscar(${contador})`);
+        $(novoitem).attr('onClick', `rabiscar($(this))`);
         $('#tarefa-definida').val('');
     })
 
@@ -21,18 +17,15 @@ $(document).ready(function() {
         $('ul h2').css("display", "none");
         $('#limpar-lista').css("display", "none");
         $('.listagem').html('');
-        contador = 0;
     })
     
 })
 
 function rabiscar(x) {
-    
-    if ($(`#${x}`).css('text-decoration-line') == 'line-through') {
-        $(`#${x}`).css('text-decoration-line', 'none');
+    if (x.css('text-decoration-line') == 'line-through') {
+        x.css('text-decoration-line', 'none');
     }
     else {
-        $(`#${x}`).css('text-decoration-line', 'line-through');
+        x.css('text-decoration-line', 'line-through');
     }
-    console.log($(`#${x}`).css('text-decoration-line'));
 }
