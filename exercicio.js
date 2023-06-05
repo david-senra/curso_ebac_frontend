77e406b4c5a27c5a9e94d2a362440a3efeae63c9
David Senra <72723579+david-senra@users.noreply.github.com>
2023-06-05 15:46:22 -0300

    Commit inicial
    Upload dos arquivos referentes ao módulo 26.

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicacao = (a, b) => a * b;
+const saudacao = (nome) => `Olá, ${nome}!`;
+const saudacao2 = (nome) => "Olá, " + nome + "!";
+console.log(multiplicacao(4, 7));
+console.log(saudacao('Pedro'));
+console.log(saudacao2('Maria'));
---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicacao = (a:number, b:number) => a * b;
+const saudacao = (nome:string) => `Olá, ${nome}!`;
+const saudacao2 = (nome:string) => "Olá, " + nome + "!";
+
+console.log(multiplicacao(4,7));
+console.log(saudacao('Pedro'));
+console.log(saudacao2('Maria'));
